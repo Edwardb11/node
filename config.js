1d9345abf5b3b9445a879288f16e0306d5c38e62
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2024-04-19 16:57:43 -0400

    featL: configure mysql

--- config.js
+++ config.js
@@ -5,4 +5,10 @@ module.exports = {
     jwt: {
         secret: process.env.JWT_SECRET || 'notasecret!',
     },
+    mysql: {
+        host: process.env.MYSQL_HOST || 'localhost', 
+        user: process.env.MYSQL_USER || 'root',
+        password: process.env.MYSQL_PASS || 'root',
+        database: process.env.MYSQL_DB || 'node',
+    },
 }
